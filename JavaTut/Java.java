b4948216d6d0d604cbb5ce36555c3d0a4632afec
Vydehi12 <Vydehishetty59@gmail.com>
2019-08-21 20:52:13 -0700

    GreensTech

---
+++ JavaTut/Java.java
@@ -0,0 +1,17 @@
+package JavaTut;
+
+public class Java {
+	String s= "Hi";
+	public void add() {
+		int a=100;
+		System.out.println("a");
+		System.out.println("s is hi");
+		
+
+	}
+	public static void main(String[] args) {
+		Java J = new Java();
+		J.add();
+	}
+
+}
---
+++ JavaTut/Java2.java
@@ -0,0 +1,8 @@
+package JavaTut;
+
+public class Java2 {
+	public void hello() {
+System.out.println("Print Hello");
+
+	}
+}
